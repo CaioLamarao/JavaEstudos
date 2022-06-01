d1b338149fdf51836c76c7b31a9dce9f7b8c5338
Caio Lamarão <caiolamarao@outlook.com>
2022-06-01 17:40:25 -0300

    Solução ideal, exceções tratadas

--- custom-exceptions/src/model/entities/Reservation.java
+++ custom-exceptions/src/model/entities/Reservation.java
@@ -4,6 +4,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+import model.exceptions.DomainExcepton;
+
 public class Reservation {
 	private Integer roomNumber;
 	private Date checkIn;
@@ -11,7 +13,10 @@ public class Reservation {
 	
 	SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
-	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) throws DomainExcepton{
+		 if(!checkOut.after(checkIn)) {
+			 throw new DomainExcepton("Error in reservation: Check-out Date must be after check-in Date.");	
+		}		
 		this.roomNumber = roomNumber;
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
@@ -38,18 +43,17 @@ public class Reservation {
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
 	
-	public String updateDates(Date checkIn, Date checkOut) {
+	public void updateDates(Date checkIn, Date checkOut) throws DomainExcepton {
 		
 		Date now = new Date();
 		if(checkIn.before(now) || checkOut.before(now)) {
-			return "Error in reservation: Reservation dates for update must be future dates";	
+			throw new DomainExcepton("Error in reservation: check-in/out must be made in future dates");	
 		}
 		 if(!checkOut.after(checkIn)) {
-			 return "Error in reservation: Check-out Date must be after check-in Date.";	
+			 throw new DomainExcepton("Error in reservation: Check-out Date must be after check-in Date.");	
 		}		
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
-		return null;
 	}
 	
 	public String toString() {
---
+++ custom-exceptions/src/model/exceptions/DomainExcepton.java
@@ -0,0 +1,10 @@
+package model.exceptions;
+
+public class DomainExcepton extends Exception {
+	private static final long serialVersionUID = 1L;
+	
+	public DomainExcepton(String msg) {
+		super(msg);
+	}
+
+}
